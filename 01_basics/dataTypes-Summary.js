cd17a85777477665d687575b3726aa3d70a1a680
Sachin Gupta <rs9055828@gmail.com>
2024-03-29 14:37:41 +0530

    dataTypes-Summary.js

---
+++ 01_basics/dataTypes-Summary.js
@@ -0,0 +1,34 @@
+//primitive
+
+// 7 Types => String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scorelive=100.3
+
+let islogged = true
+let outSideTemp= null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 3482085832749983n
+
+//console.log(typeof bigNumber);
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+let heros = ['Shatkimaan, Nagraj, Doga, Krishna, Hanumen']
+
+let Obj ={
+    name:'Sahin',
+    age:23,
+    bloodGrp :'0-'
+}
+
+const myFunction = function(){
+    console.log('Jai Shri Ram');
+}
