76fc6c85f1afed5fa11efc5056bfcadd595dfeac
Sachin Gupta <rs9055828@gmail.com>
2024-03-29 17:33:33 +0530

    05_string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = 'sachin'
+const age= 25
+
+//console.log(`Namaste this is ${name} and my age is ${age}`);
+
+const rich = new String('Yes i`m Billionaire yaa')
+
+//console.log(rich.slice(9,18)); //the end section are not execute but start and position of end`s before index will execute 
+
+//console.log(rich.search('i')); // it return indexof
+
+console.log(rich.repeat('yes')); // not execute
