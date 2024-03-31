46efa7976eb20513b4a60622a6d93e0b928e0355
Sachin Gupta <161197824+SachinGupta369@users.noreply.github.com>
2024-03-31 19:20:45 +0000

    06_numsAndMAths.js

--- 01_basics/06_numsAndMaths.js
+++ 01_basics/06_numsAndMaths.js
@@ -16,3 +16,20 @@ const digits = 100000000
 
 //++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
 
+// console.log(Math);
+// console.log(Math.round(39.9));
+// console.log(Math.abs(-9));
+// console.log(Math.ceil(6.2));
+// console.log(Math.floor(6.9));
+// console.log(Math.min(1,23,4,5,6));
+// console.log(Math.max(1,23,4,5,6));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log((Math.floor(Math.random()*10)+1))
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()*(min-max+1))+min);
