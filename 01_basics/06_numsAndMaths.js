6a41b2b11987b4fdf847ce13ffeb222a8ee625fb
Sachin Gupta <161197824+SachinGupta369@users.noreply.github.com>
2024-03-31 18:22:22 +0000

    06_numsAndMaths.js

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,18 @@
+const score = 100
+
+let number = new Number(100)
+//console.log(number);
+//console.log(typeof number);
+
+//console.log(number.toString().length);
+
+//console.log(number.toFixed(19));
+
+const numberss = 123.56
+//console.log(numberss.toPrecision(7));
+
+const digits = 100000000
+//console.log(digits.toLocaleString('en-in'));
+
+//++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
