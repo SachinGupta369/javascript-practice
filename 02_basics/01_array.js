c9d968784374fb21fca08e81b0ad6cc5d912cca3
Sachin Gupta <sachingupta0098@gmail.com>
2024-04-09 16:54:22 +0530

    01_arrays.js & 04_functions.js

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -5,14 +5,14 @@ const newarr = new Array(1,2,3,4)
 //console.log(newarr[0]);
 
 //Array Methods
-const myarr = [0,1,2,3,4,5]
+const myarr = [-1,1,2,3,4,5]
 // myarr.push(6)
 // myarr.pop()
 // console.log('a',myarr);
 
 //slice and splice
 
-const spilce = myarr.splice(1,3)
+//const spilce = myarr.splice(1,3)
 // console.log('b',spilce); 
 // console.log(myarr);
 // splice remove the mention index and make it different array
@@ -29,9 +29,10 @@ The slice() method selects from a given start, up to a (not inclusive) given end
 The slice() method does not change the original array.
 */
 
-// myarr.shift(0)
-// myarr.unshift(9)
-// console.log(myarr);
+//console.log(myarr);
+ //myarr.shift(0)
+//myarr.unshift(9)
+ //console.log(myarr);
 
 //console.log(myarr.includes(9) );
 // const arre = myarr.join() //join() also makes arrays in string
@@ -39,9 +40,34 @@ The slice() method does not change the original array.
 // console.log(typeof arre);
 
 
-//Splice 
+//Splice => In Splice the array start from 1 not from Zero 
 
 let arr = [1,2,3,4,5,6]
 
 // add Element 
-console.log(arr.splice(2,0,'100','101'))
+//arrs.splice(2,0,369)
+//arr.splice(1,0,99,108)
+
+// the last 3 number removed the 3 element after 2 index
+//arr.splice(2,3)
+
+// Remove and Replace the value
+//arr.splice(1,4,99,'Om Namah Shivaya')
+
+//splice(4) this will delete the element after 4 and return only on calling
+// arr.splice(4)
+// console.log(arr);
+
+//Practice Question by Shraddha Khapra
+
+let companies = ["Bloomberg","Microsoft","Uber","Google", "IBM", "NETFLIX"]
+
+companies.shift(0)
+console.log(companies);
+
+companies.splice(1,1,"Ola")//starting Parameter idx 1, middle Parameter idx 1 is for Remove, Add element Last Parameter 
+console.log(companies);
+
+companies.unshift("NewsBerg") // add element at 0 idx
+companies.push("Amazon") //add element the Last of the Array
+console.log(companies);
---
+++ 02_basics/04_functions.js
@@ -0,0 +1,14 @@
+//Function with Parameter
+function sum(a,b){
+    console.log(a+b);
+}
+sum(2,2)
+
+//Function with parameter and Return word
+
+function minus(a,b){
+    r=a-b;
+    return r
+}
+let ret= minus(4,2)
+console.log(ret);
