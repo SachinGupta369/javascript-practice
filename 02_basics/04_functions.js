01bc183e388f01fdeff78834fc1336b1013fc943
Sachin Gupta <sachingupta0098@gmail.com>
2024-04-13 23:28:34 +0530

    04_functions.js

--- 02_basics/04_functions.js
+++ 02_basics/04_functions.js
@@ -2,7 +2,7 @@
 function sum(a,b){
     console.log(a+b);
 }
-sum(2,2)
+//sum(2,2)
 
 //Function with parameter and Return word
 
@@ -11,4 +11,22 @@ function minus(a,b){
     return r
 }
 let ret= minus(4,2)
-console.log(ret);
+//console.log(ret);
+
+// Practiceset by Arrow Functions
+
+let multii = (c,b) =>{
+     s = c*b;
+     return s;
+}
+
+let ansmulti= multii(5,4);
+//console.log(ansmulti);
+
+//Create a function using "Function" keyword that takes String as a argument & returns number of Vowels in a string
+
+function numberOfVowels(str){
+    for(let vow of str){
+        console.log(vow);
+    }
+}
