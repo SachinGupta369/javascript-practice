eb2a896c9a4f93f33fcdeff54d17198cf0439b15
Sachin Gupta <sachingupta0098@gmail.com>
2024-05-04 17:57:44 +0530

    04_function.js, index.html

--- 02_basics/04_functions.js
+++ 02_basics/04_functions.js
@@ -74,6 +74,97 @@ let aa = ["Farzeen", "Priti", "Baljinder"];
 // });
 
 // with arrow function 
-aa.forEach((val)=>{
- console.log(val.concat(" Loves to Sachin"));
+// aa.forEach((val)=>{
+//  console.log(val.concat(" Loves to Sachin"));
+// })
+
+
+/*
+ForEach            //what is Higher Order Functions?
+*/
+let arr = ['sandesh', 'mohit', 'ram', 'narayan']
+
+arr.forEach((num)=>{
+  //console.log(num.toUpperCase().charAt(1));
 })
+
+/*
+For the given array of the numbers, print the square of each value using the forEach loop
+*/
+
+// let loop = [1,2,3,4,5]
+// loop.forEach(s=>{
+//   console.log(ssl)
+// })
+ // o/p: undefined
+
+/*
+Use of Map on array  it creates new array instead of using original one
+*/
+
+let num = [2,3,4,5]
+
+let nums = num.map((s)=>{
+  return s*s
+})
+
+//console.log(nums);
+
+/*
+Filter 
+*/
+
+let nummy =[2, 3, 4, 5,6,34,54,31,32,99]
+
+let evenArr = nummy.filter((val)=>{
+  return val%2===0;
+})
+
+//console.log(evenArr);
+
+/*
+Reduce in Array method
+*/
+
+let reduceArr = [2,3,6,45,4,4,0]
+
+let reduceMet = reduceArr.reduce((pre,curr)=>{
+  //return pre+curr
+  //return pre > curr ? curr : pre; // 0
+  return pre > curr ? pre : curr; // 45
+})
+
+//console.log(reduceMet);
+
+/*
+  we are given array of marks of the student, filter out the student who has score more than 90+ 
+*/
+
+let marksOfStudent = [34,54,65,97,99,84,90,89.99,92]
+
+let resultOfStudent = marksOfStudent.filter((result)=>{
+  return result > 90
+})
+
+//console.log(resultOfStudent);
+
+/*
+    Take a number n as a input from user. Create a array from 1 to n. use of reduce method to calculate sum of all
+    numbers in the array. use of reduce method to calculate product of all numbers in the array
+*/
+
+let n = prompt("Enter a Number: ")
+
+let arrays = []
+
+for(let i=1;i<=n;i++){
+  arrays[i-1]=i;
+}
+
+console.log(arrays);
+
+let sums = arrays.reduce((pre,curr)=>{
+  return pre*curr
+})
+
+console.log(sums);
