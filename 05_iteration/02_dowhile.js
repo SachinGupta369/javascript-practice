e6aa7a7acb1a1b1fb1c172c0661e636db1917912
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-12 21:56:26 +0530

    do while.js

---
+++ 05_iteration/02_dowhile.js
@@ -0,0 +1,20 @@
+let age = 0
+
+while(age<=10){
+    // console.log(`Age is ${age}`);
+    age= age +1   
+}
+
+let array = ['Ram', 'Krishna', 'Narayan','Shiva', 'Rudra']
+
+let omNamah = 0
+while(omNamah<array.length){
+    // console.log(`Om Namaha ${array[omNamah]}`);
+    omNamah = omNamah+1
+}
+
+let no=100
+do{
+    console.log(`Print no ${no} `);
+    no= no+1
+}while(no<11)
