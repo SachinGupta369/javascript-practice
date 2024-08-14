10dcd41dbeea94bf2e29ebd5de93d65ad6a9ec68
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-14 12:30:30 +0530

    03 nullish & ternary

---
+++ 05_iteration/03_nullish,Ternary.js
@@ -0,0 +1,23 @@
+// Nullish Coalescing Operator (??):null undefined
+
+let val1;
+val1 = 5 ?? 10
+// console.log(val1);
+
+val2 = null ?? 10 
+// console.log(val2);
+
+val1 = undefined ?? 5
+// console.log(val1);
+
+val1 = null ?? 10 ?? 20
+// console.log(val1);
+
+// ternary operator
+
+const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log(`less than 80`) : console.log(`more than 80`);
+
+
+
+
