3614cee67804a68a208b40a287c8442244684a18
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-14 12:11:59 +0530

    01 truthy falsy.js

---
+++ 05_iteration/01_Truthy_falsy.js
@@ -0,0 +1,25 @@
+userEmail = []
+if(userEmail.length == 0){
+    // console.log(`Empty array value is equal to`);
+}else{
+    // console.log('Not equal to ');
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined , NaN
+
+// truthy values
+
+// "0", 'false', " ", [], {}, function() {}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj.length === 0)){
+    // console.log('emptyObj is equal to Zero');
+}
+
+console.log(false==0);
+console.log(false=='');
+console.log(0 =='');
+console.log("" === '');
