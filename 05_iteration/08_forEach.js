a3da3838e3b15f935c56a5d1ab3fa7b27dae6eba
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-15 20:37:16 +0530

    08 for each.js

---
+++ 05_iteration/08_forEach.js
@@ -0,0 +1,37 @@
+const coding = ['js', 'ruby', 'java', 'python', 'cpp']
+
+// coding.forEach( function name(vsl){
+//     // console.log(vsl)
+// })
+
+// coding.forEach((items)=>{
+//     console.log(items);
+// })
+
+function extentions(coding){
+    // console.log(coding);
+}
+
+coding.forEach(extentions)
+
+coding.forEach((idx,item,Array)=>{
+    // console.log(idx,item,Array);
+})
+
+const code =[{
+    lang : 'Java',
+    ext : 'java'
+},
+{
+    lang : 'Python',
+    ext : 'py'
+},
+{
+    lang : 'Javascript',
+    ext : 'js'
+}]
+
+code.forEach((val)=>{
+    console.log(val.ext);
+    
+})
