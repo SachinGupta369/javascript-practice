436eea87d98f9a8d37e2df88e518ab6d926d78b9
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-27 13:17:31 +0530

    03 timer

---
+++ 07_projects/03_time/app.js
@@ -0,0 +1,11 @@
+let time = document.querySelector('#time')
+
+setInterval(function(){
+    let date = new Date();
+    time.innerHTML = date.toLocaleTimeString()
+    console.log(date.toLocaleTimeString());
+    
+},1000)
+
+
+
